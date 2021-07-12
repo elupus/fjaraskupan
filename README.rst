964194619fc3f36b98b49bad616af48d29f53e7d
Joakim Plate <elupus@ecce.se>
2021-07-12 11:38:08 +0200

    Add initial structure

---
+++ README.rst
@@ -0,0 +1,25 @@
+********************************
+Fjäråskupan Bluetooth Control
+********************************
+This module support controlling Fjäråskupan kitchen fans over bluetooth
+
+Status
+______
+
+.. image:: https://github.com/elupus/fjaraskupan/actions/workflows/python-package.yml/badge.svg
+    :target: https://github.com/elupus/fjaraskupan
+
+Module
+======
+
+Code to set volume and source using library.
+
+.. code-block:: python
+
+
+    async def run():
+        pass
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete (run())
+
---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages, setup
+
+# read the contents of your README file
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.rst"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="fjaraskupan",
+    version="0.1.0",
+    description="A python library for speaking to fjäråskupan",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    license="MIT",
+    packages=["fjaraskupan"],
+    package_dir={"": "src"},
+    python_requires=">=3.8",
+    author="Joakim Plate",
+    install_requires=["bleak"],
+    extras_require={
+        "tests": [
+            "pytest>3.6.4",
+            "pytest-mock",
+        ]
+    },
+    url="https://github.com/elupus/fjaraskupan",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Framework :: AsyncIO",
+    ],
+)
